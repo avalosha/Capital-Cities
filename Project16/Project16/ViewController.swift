0cb911ce54ba3c767db37dc636231d9207eb4eda
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2020-08-07 21:10:05 -0500

    Setup project.

---
+++ Project16/Project16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Álvaro Ávalos Hernández on 07/08/20.
+//  Copyright © 2020 Álvaro Ávalos Hernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
